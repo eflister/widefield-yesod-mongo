3f5314128da1b51c6a8218a435b8fc0864fae9ce
erik <erik.flister@gmail.com>
2012-10-24 16:50:29 -0700

    compiles :)

--- Handler/People.hs
+++ Handler/People.hs
@@ -12,76 +12,96 @@ getPeopleR :: Handler RepHtml
 getPeopleR = undefined
 
 getPersonR :: PersonId -> Handler RepHtml
-getPersonR = gridForm False peopleGrid
+getPersonR = undefined -- gridForm False peopleGrid
 
 postPersonR :: PersonId -> Handler RepHtml
-postPersonR = gridForm True peopleGrid
+postPersonR = undefined -- gridForm True peopleGrid
 
-peopleGrid :: Maybe PersonId 
-           -> GHandler App App a
-
+-- supply Person-specific details to gridForm
+peopleGrid :: (ToWidget App App a)
+           => Maybe PersonId 
+           -> GHandler App App ( GHandler App App (Markup -> MForm App App (FormResult [Int], a))
+                               , (PersonId -> Route App)
+                               , Route App
+                               , [GridField]
+                               )
 peopleGrid = itemGrid PersonR PeopleR . getZipList $ GridField 
-    <$> ZipList ["name"                                       , "age"                                     ] 
-    <*> ZipList [personName                                   , show . personAge                          ] -- make this line existential somehow?  Show s => [Person -> s]
---  <*> ZipList [Just $ EditableRec textField PersonName False, Just $ EditableRec intField PersonAge True]
-    <*> ZipList [Nothing                                      , Just $ EditableRec intField PersonAge True] -- fields not returning same type can't be in same list :(
-    <*> ZipList [False                                        , True                                      ]
+    <$> ZipList ["name"                                    , "age"                                  ] 
+    <*> ZipList [personName                                , show . personAge                       ] -- make this line existential somehow?  Show s => [Person -> s]
+--  <*> ZipList [Just $ Editable textField PersonName False, Just $ Editable intField PersonAge True]
+    <*> ZipList [Nothing                                   , Just $ Editable intField PersonAge True] -- fields not returning same type can't be in same list :(
+    <*> ZipList [False                                     , True                                   ]
 
-data GridField a b c = 
+data GridField = 
     GridField { label    :: Text
-              , extract  :: a -> String
-              , editable :: Maybe (EditableRec b c) 
+              , extract  :: Person -> String
+              , editable :: Maybe Editable
               , needShow :: Bool
               }
 
-data EditableRec b c =
-	EditableRec { htmlField :: forall sub master . (RenderMessage master FormMessage, Integral b) => Field sub master b
-                , dbField   :: EntityField Person c
-                , required  :: Bool
-                }
+data Editable =
+	Editable { htmlField :: Field App App Int
+             , dbField   :: EntityField Person Int
+             , required  :: Bool
+             }
 
-itemGrid :: (RenderMessage master FormMessage)
+-- lookup all items of a given type in the database
+itemGrid :: ( ToWidget App App b
+            )
          => (PersonId -> Route App)
          -> Route App
-         -> [GridField Person b c]
+         -> [GridField]
          -> Maybe PersonId
-         -> GHandler sub master a
-itemGrid = undefined
-{-
+         -> GHandler App App ( GHandler App App (Markup -> MForm App App (FormResult [Int], b))
+                             , (PersonId -> Route App)
+                             , Route App
+                             , [GridField])
 itemGrid indR groupR fields sel = do
-    items <- lift . runDB $ selectList [] []
-    -- liftIO . mapM_ (putStrLn . show) $ entityKey <$> items
+    let items = undefined
+    -- items <- {- lift . -} runDB $ selectList [] []
+    liftIO . mapM_ (putStrLn . show) $ entityKey <$> items
     let grid = makeGrid indR groupR sel items fields
     return (grid, indR, groupR, fields)
--}
-         
-gridForm :: () --(RenderMessage master FormMessage)
+
+-- run a form, extract results, and update the database
+gridForm :: ( ToWidget App App b
+            ) --(RenderMessage master FormMessage)
          => Bool
-         -> (Maybe PersonId -> GHandler App App ((GHandler App App (Markup -> MForm App App (FormResult [a], b))), (PersonId -> Route App), Route App, [GridField Person b a]))
+         -> (Maybe PersonId -> GHandler App App ( GHandler App App (Markup -> MForm App App (FormResult [Int], b))
+                                                , (PersonId -> Route App)
+                                                , Route App
+                                                , [GridField]
+                                                ))
          -> PersonId
          -> Handler RepHtml
---gridForm = undefined
 gridForm post gridder pid = do
     (grid, indR, groupR, fields) <- gridder $ Just pid
     let done w e = defaultLayout [whamlet|
 $# this form tag closes immediately, can it not cross other tags?
 <form method=post action=@{indR pid} enctype=#{e}>
     ^{w}
-|]    
+|]   
     if post 
         then do
             ((r, w), e) <- runFormPost =<< grid -- will this give results for optional and untouched fields?  we depend on the order, so...
             case r of
                 FormSuccess rs -> do
-                    runDB $ update pid . getZipList $ ZipList ((=.) <$> dbField <$> mapMaybe editable fields) <*> ZipList rs -- [PersonAge =. age]
+                    runDB $ update pid . getZipList $ ZipList ((=.) <$> dbField <$> mapMaybe editable fields) <*> ZipList rs
                     setMessage "success"
-                    redirect groupR
+                    redirect groupR 
                 _ -> done w e -- use contents of failure somehow?  does fvErrors already get it all?
         else do
             (w, e) <- generateFormPost =<< grid
             done w e
-
-
+        
+-- generate a grid showing the fields for items passed in, possibly including a form for editing a selected one
+makeGrid :: (ToWidget App App b)
+         => (PersonId -> Route App)
+         -> Route App
+         -> Maybe PersonId
+         -> [Entity Person]
+         -> [GridField]
+         -> GHandler App App (Markup -> MForm App App (FormResult [Int], b))
 makeGrid = undefined
 {-
 makeGrid indR groupR sel items fields = 
@@ -122,7 +142,9 @@ $forall f <- fields
                 $nothing
                     ^{dispRow i}                                
 |]
+-}
 
+{-
 personAgeMForm :: (PersistStore (YesodPersistBackend master) (GHandler sub master), YesodPersist master, RenderMessage master FormMessage) 
      => Key (YesodPersistBackend master) (PersonGeneric backend) -- PersonId
      -> GHandler sub master (Html -> MForm sub master (FormResult Int, GWidget sub master ()))
