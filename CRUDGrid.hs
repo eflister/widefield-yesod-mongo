fc3410ad9f8deca66f8917c7a66c8c4a5d186500
erik <erik.flister@gmail.com>
2012-11-12 23:41:29 -0800

    form out of table

--- CRUDGrid.hs
+++ CRUDGrid.hs
@@ -23,8 +23,6 @@ import Data.Maybe
 import Prelude (head)
 import Data.List.Split
 import qualified Data.Text as T
-import Text.Blaze.Renderer.String
-import Text.Blaze
 
 type ID m p = Key (YesodPersistBackend m) p
 
@@ -159,27 +157,155 @@ gridForm run g sel = do
         rts     = routes g
         goGroup = redirect $ groupR rts
         postR   = (const (newR rts) ||| (indR rts . fromJust)) sel 
-
-    items <- runDB . selectList [] $ opts g
-
-    let False +> _ = Nothing
+        False +> _ = Nothing
         True  +> x = x
         (allowActions, title') = case sel of 
             Left        _  -> (False, "create new"    )
             Right (Just _) -> (False, "edit item"     )
             Right Nothing  -> (True , toHtml $ title g)
-        (dels, edits) = first (allowDelete g +>) $ join (***) (allowActions +>) (Just $ deleteR rts, Just $ indR rts)
+        (dels, edits) = first (allowDelete g +>) . join (***) ((allowActions +>) . Just) $ (deleteR &&& indR) rts
 
-    rows <- mapM (makeRow fields dels edits) items
+        done x = defaultLayout $ do
+            setTitle title'
+            case x of
+                Left w -> w
+                Right (w, e) -> [whamlet|
+<form method=post action=@{postR} enctype=#{e}>
+    ^{w}
+|]
+            when (isJust (defaultNew g) && (const False ||| isNothing) sel)
+                [whamlet|
+<a href=@{newR rts}> 
+        <input type="button" value="new">
+|]
 
+    items  <- runDB . selectList [] $ opts g
+    rows   <- mapM (makeRow fields dels edits) items
     gridID <- newIdent
 
-    let done w e = defaultLayout $ do
-            setTitle title'
-            let htmlGrid = makeGrid postR sel fields rows w e gridID
-            case jsgrid g of
-                        Nothing -> htmlGrid $ Just "border-spacing: 20px 0; border-collapse: separate"
-                        Just (DGrid script attrs) -> do
+    let sel' = right ((\y -> filter ((y ==) . entityKey) items) <$>) sel
+        
+    case sel' of Right (Just []     ) -> setMessage noneFoundMsg >> goGroup
+                 Right (Just (_:_:_)) -> setMessage "error: multiple matches with that id"
+                 _ -> return undefined
+
+    case editForm (groupR rts) fields rows sel gridID (jsgrid g) $ (Just ||| (entityVal . head <$>)) sel' of 
+        Left  grid -> done . Left $ grid
+        Right form -> if run 
+            then do
+                ((r, w), e) <- runFormPost form
+                case r of 
+                    FormSuccess p -> do
+                        flip (either . const . void . runDB $ insert p) sel $ \(Just pid) -> do -- how tell if insert succeeded?
+                            let convert (GridField _ extract' _ (Just (Editable _ pField' True _))) = Just $ pField' =. extract' p
+                                convert _ = Nothing
+                            runDB . update pid . catMaybes $ convert . getField g <$> [minBound..maxBound] -- how tell if update succeeded?                                  
+                        setMessage "success"
+                        redirect $ groupR rts -- goGroup    
+                    _ -> done $ Right (w, e) -- use contents of failure somehow?  does fvErrors already get it all?
+            else do
+                (w, e) <- generateFormPost form
+                done $ Right (w, e)
+
+makeGrid :: ( PersistEntity p
+            , PersistQuery (YesodPersistBackend m) (GHandler s m)
+            , YesodPersist m
+            , Bounded c
+            , Enum c
+            , Eq c
+            , PersistEntityBackend p ~ YesodPersistBackend m
+            , RenderMessage m FormMessage
+            ) 
+         => Either p (Maybe (ID m p)) 
+         -> [(c, GridField s m p c)]
+         -> [(ID m p, GWidget s m (), [(String, (Maybe (Route m), String))])]
+         -> Text                  
+         -> Maybe p         
+         -> Route m
+         -> Maybe (c -> Maybe (FieldView s m))
+         -> Maybe Text
+         -> Maybe Html 
+         -> GWidget s m ()
+makeGrid sel fields rows gridID this groupR' getView tstyle extra = case tstyle of
+    Nothing -> dgrid gridID fields rows sel getView
+    Just tstyle' -> do
+        let style = [lucius| .errors { color:red } |]
+            form  = [whamlet|
+$forall (c, f) <- fields
+    <td>
+        $maybe view <- (fromJust getView) c
+            $#adapted from renderDivs
+            <div :fvRequired view:.required :not $ fvRequired view:.optional>
+                <label for=#{fvId view}>
+                    $maybe tt <- fvTooltip view
+                        <div .tooltip>#{tt}
+                    ^{fvInput view}
+                    $maybe err <- fvErrors view
+                        <div .errors >#{err}
+        $nothing
+            ^{toWhamlet $ mini f (fromJust this)}
+<td>
+    <input type="submit" value="save">
+<td>
+    <a href=@{groupR'}> 
+        <input type="button" value="cancel">    
+|]                
+        [whamlet|
+^{style}
+$maybe ex <- extra
+    ^{ex}
+<table style=#{tstyle'} id=#{gridID}>
+    <thead>
+        <tr>
+            $forall (c, f) <- fields
+                <th> #{heading f $ c}
+            $# <th> actions
+            $if tstyle' == ""
+                <th> edit
+                <th> delete
+    <tbody>
+        $forall (rkey, row, _) <- rows
+            <tr>        
+                $maybe key <- (const Nothing ||| id) sel
+                    $if key == rkey
+                        ^{form}
+                    $else
+                        ^{row}
+                $nothing
+                    ^{row}  
+        $if (const True ||| const False) sel
+            <tr>
+                ^{form}
+|]
+            
+editForm :: ( PersistEntity p
+            , PersistQuery (YesodPersistBackend m) (GHandler s m)
+            , YesodPersist m
+            , Bounded c
+            , Enum c
+            , Eq c
+            , PersistEntityBackend p ~ YesodPersistBackend m
+            , RenderMessage m FormMessage
+            ) 
+         => Route m
+         -> [(c, GridField s m p c)]
+         -> [(ID m p, GWidget s m (), [(String, (Maybe (Route m), String))])]
+         -> Either p (Maybe (ID m p)) 
+         -> Text
+         -> Maybe (JSGrid m)
+         -> Maybe p
+         -> Either (GWidget s m ()) 
+                   (Html -> MForm s m ( FormResult p
+                                      , GWidget s m ()
+                                      )
+                   )
+editForm groupR' fields rows sel gridID jsgrid' this' = do
+    let w getView = do
+        let htmlGrid = makeGrid sel fields rows gridID this' groupR' getView
+        case jsgrid' of
+            Nothing -> htmlGrid $ Just "border-spacing: 20px 0; border-collapse: separate"
+{-            
+            Just (DGrid script attrs) -> do
                             addScriptAttrs script attrs
 {-
     let c = T.unlines
@@ -207,11 +333,12 @@ gridForm run g sel = do
         -}
 
     addScriptRemoteAttrs "//ajax.googleapis.com/ajax/libs/dojo/1.8.0/dojo/dojo.js" [("data-dojo-config",c)] -- 1.8.1 is 404'ing
--}                            
+-}  
                             htmlGrid Nothing
-                        Just (JQGrid (sheets, scripts)) -> do
-                            mapM_ addStylesheet sheets
-                            toWidget [lucius|
+-}
+            Just (JQGrid (sheets, scripts)) -> \extra -> do
+                mapM_ addStylesheet sheets
+                toWidget [lucius|
 html, body {
     margin: 0;
     padding: 0;
@@ -219,46 +346,21 @@ html, body {
 }
 .ui-jqgrid tr.jqgrow td {white-space:normal;}
 |]
-                            addScriptRemote "//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"
-                         -- addScriptRemote "//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"
-                            mapM_ addScript scripts
-                            toWidget [julius| 
+                addScriptRemote "//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"
+             -- addScriptRemote "//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"
+                mapM_ addScript scripts
+                toWidget [julius| 
 $(document).ready(function() {
     jQuery.extend(jQuery.jgrid.defaults, { altRows:true, height:"auto" });
-    tableToGrid("##{gridID}", { shrinkToFit:true, width:350  }) 
+    tableToGrid("##{gridID}", { shrinkToFit:true, width:600  }) 
 });                
 |]
-                            htmlGrid $ Just ""
-            when (isJust (defaultNew g) && (const False ||| isNothing) sel)
-                [whamlet|
-<a href=@{newR rts}> 
-        <input type="button" value="new">
-|]
-
-    let sel' = right ((\y -> filter ((y ==) . entityKey) items) <$>) sel
-        
-    case sel' of Right (Just []     ) -> setMessage noneFoundMsg >> goGroup
-                 Right (Just (_:_:_)) -> setMessage "error: multiple matches with that id"
-                 _ -> return undefined
-
-    case editForm (groupR rts) fields <$> (Just ||| (entityVal . head <$>) $ sel') of 
-        Nothing -> done undefined undefined
-        Just form ->
-            if run 
-                then do
-                    ((r, w), e) <- runFormPost form
-                    case r of 
-                        FormSuccess p -> do
-                            flip (either . const . void . runDB $ insert p) sel $ \(Just pid) -> do -- how tell if insert succeeded?
-                                let convert (GridField _ extract' _ (Just (Editable _ pField' True _))) = Just $ pField' =. extract' p
-                                    convert _ = Nothing
-                                runDB . update pid . catMaybes $ convert . getField g <$> [minBound..maxBound] -- how tell if update succeeded?                                  
-                            setMessage "success"
-                            redirect $ groupR rts -- goGroup    
-                        _ -> done w e -- use contents of failure somehow?  does fvErrors already get it all?
-                else do
-                    (w, e) <- generateFormPost form
-                    done w e
+                htmlGrid (Just "") extra
+    case this' of 
+        Nothing   -> Left $ (w Nothing) Nothing
+        Just this -> Right $ \extra -> do
+            (rs, vs) <- getDefaultedViews fields this
+            return (rs, w (Just $ fromJust . (`lookup` vs)) $ Just extra)
 
 makeRow :: ( Bounded c
            , Enum c
@@ -299,62 +401,6 @@ $maybe indR' <- allowEdit
     let jsob = ((\(c,f) -> heading f $ c) &&& (flip disp i . snd)) <$> fields
     return (entityKey i, widget, jsob)
 
-makeGrid :: ( Bounded c
-            , Enum c
-            , Eq c
-            , PersistEntity p
-            , PersistQuery (YesodPersistBackend m) (GHandler s m)
-            , YesodPersist m
-            , Yesod m
-            , RenderMessage m FormMessage
-            , PersistEntityBackend p ~ YesodPersistBackend m
-            )
-         => Route m
-         -> Either p (Maybe (ID m p))
-         -> [(c, GridField s m p c)]
-         -> [(ID m p, GWidget s m (), [(String, (Maybe (Route m), String))])]
-         -> GWidget s m ()
-         -> Enctype
-         -> Text
-         -> Maybe Text 
-         -> GWidget s m ()
-makeGrid postR sel fields rows w e gid tstyle = do
-    let style = [lucius| .errors { color:red } |]
-        form  = [whamlet|
-<form method=post action=@{postR} enctype=#{e}>
-    ^{w}
-|]
-        grid = case tstyle of
-            Just tstyle' -> [whamlet|
-<table style=#{tstyle'} id=#{gid}>
-    <thead>
-        <tr>
-            $forall (c, f) <- fields
-                <th> #{heading f $ c}
-            $# <th> actions
-            $if tstyle' == ""
-                <th> edit
-                <th> delete
-    <tbody>
-        $forall (rkey, row, _) <- rows
-            <tr>        
-                $maybe key <- (const Nothing ||| id) sel
-                    $if key == rkey
-                        ^{form}
-                    $else
-                        ^{row}
-                $nothing
-                    ^{row}  
-        $if (const True ||| const False) sel
-            <tr>
-                ^{form}
-|]
-            Nothing -> dgrid gid fields rows sel form
-    [whamlet|
-^{style}
-^{grid}
-|]
-
 mini (GridField _ extract' display' _) = ((((\x -> (Nothing, x)) .) ||| id) display') . extract'
 
 showFieldByID :: ( YesodPersist m
@@ -383,48 +429,6 @@ toJulius (r, x) = case r of
     Nothing    -> [julius|                   escapeHTML("#{x}") |]
     Just route -> [julius| <a href=@{route}> escapeHTML("#{x}") |]
 
-editForm :: ( PersistEntity p
-            , PersistQuery (YesodPersistBackend m) (GHandler s m)
-            , YesodPersist m
-            , Bounded c
-            , Enum c
-            , Eq c
-            , PersistEntityBackend p ~ YesodPersistBackend m
-            , RenderMessage m FormMessage
-            ) 
-         => Route m
-         -> [(c, GridField s m p c)]
-         -> p
-         -> Html 
-         -> MForm s m ( FormResult p
-                      , GWidget s m ()
-                      )
-editForm groupR' fields this extra = do
-    (rs, vs) <- getDefaultedViews fields this
-    let getView = fromJust . (`lookup` vs)
-        w = [whamlet|
-^{extra}
-$forall (c, f) <- fields
-    <td>
-        $maybe view <- getView c
-            $#adapted from renderDivs
-            <div :fvRequired view:.required :not $ fvRequired view:.optional>
-                <label for=#{fvId view}>
-                    $maybe tt <- fvTooltip view
-                        <div .tooltip>#{tt}
-                    ^{fvInput view}
-                    $maybe err <- fvErrors view
-                        <div .errors >#{err}
-        $nothing
-            ^{toWhamlet $ mini f this}
-<td>
-    <input type="submit" value="save">
-<td>
-    <a href=@{groupR'}> 
-        <input type="button" value="cancel">
-|]
-    return (rs, w)
-
 getDefaultedViews :: ( RenderMessage m FormMessage
                      )
                   => [(c, GridField s m p c)]
@@ -441,13 +445,13 @@ getDefaultedViews fields this = do
                 _ -> return (                   mp, Nothing)
     foldM defView (pure this, []) fields
 
-dgrid ::    Text
+dgrid    :: Text
          -> [(c, GridField s m p c)]
          -> [(ID m p, GWidget s m (), [(String, (Maybe (Route m), String))])]
          -> Either p (Maybe (ID m p))
+         -> Maybe x
          -> GWidget s m ()
-         -> GWidget s m ()
-dgrid gridID fields rows sel form = do
+dgrid gridID fields rows sel getView = do 
     let -- could probably use http://hackage.haskell.org/packages/archive/aeson/latest/doc/html/Data-Aeson.html#t:ToJSON
         gData   = mconcat $ (\(_,_,r) -> [julius|                   {^{doRow r}                                       }, |]) <$> rows
         doRow r = mconcat $ (\(c,f)   -> [julius| #{heading f $ c}:  ^{toJulius (fromJust $ lookup (heading f $ c) r) }, |]) <$> fields
@@ -458,7 +462,6 @@ dgrid gridID fields rows sel form = do
                      #{heading f $ c}:"placeholder",
                  },
 -}       
-        -- how get into the guts of a widget?  renderMarkup . toMarkup . pageBody ??? $ widgetToPageContent form
         gCols = mconcat $ (\(c,f) -> [julius| #{heading f $ c}: { label: "#{heading f $ c}"
                                                                 , renderCell: function (row, value, td, options){
                                                                     td.innerHTML = value; //so links render
